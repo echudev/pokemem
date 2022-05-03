6e125a7b1600eab88adfd0b7f666cdfcdcd08a37
Ezequiel <ezequiel.maranda@gmail.com>
2022-05-03 17:58:31 -0300

    cambio BrowserRouter x HashRouter

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Home } from "../views/Home";
 import { Game1 } from "../views/Game1";
 
 function AppRoutes() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/Game1" element={<Game1 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default AppRoutes;
