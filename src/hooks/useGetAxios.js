bf8ef1b2caba564ff3f5f457cc9cf2cf41bfc74a
Ezequiel <ezequiel.maranda@gmail.com>
2022-10-01 12:31:40 -0300

    eslint added

--- src/hooks/useGetAxios.js
+++ src/hooks/useGetAxios.js
@@ -1,38 +1,36 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios';
+import axios from 'axios'
 
-export const useGetAxios = (url,func1) => {
+export const useGetAxios = (url, func1) => {
+  // se crea un estado para guardar la informacion de la api
+  const [data, setData] = useState({
+    loading: true,
+    data: null,
+    error: null
+  })
 
-    //se crea un estado para guardar la informacion de la api
-    const [data, setData] = useState({
-        loading: true,
-        data: null,
+  const getData = async (url) => {
+    try {
+      const response = await axios(url)
+      setData({
+        loading: false,
+        data: response.data,
         error: null
-    });
-
-    const getData = async (url) => {
-        try {
-            const response = await axios(url);
-            setData({
-                loading: false,
-                data: response.data,
-                error: null
-            });
-        }
-        catch (error) {
-            setData({
-                loading: false,
-                data: null,
-                error: error
-            });
-            console.log(error)
-        }
+      })
+    } catch (error) {
+      setData({
+        loading: false,
+        data: null,
+        error
+      })
+      console.log(error)
     }
+  }
 
-    //se ejecuta una sola vez al inicio del componente
-    useEffect(() => {
-        getData(url);
-    }, [func1]);
+  // se ejecuta una sola vez al inicio del componente
+  useEffect(() => {
+    getData(url)
+  }, [func1])
 
-    return [data.loading, data.data, data.error];
+  return [data.loading, data.data, data.error]
 }
--- src/hooks/useGetCards.js
+++ src/hooks/useGetCards.js
@@ -1,60 +1,56 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { generateRandom } from "../utils/generateRandom";
-import { shuffleArray } from "../utils/shuffleArray";
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { generateRandom } from '../utils/generateRandom'
+import { shuffleArray } from '../utils/shuffleArray'
 
 export const useGetCards = (n = 6, handler) => {
-    const [data, setData] = useState({
-        loading: true,
+  const [data, setData] = useState({
+    loading: true,
+    error: null,
+    cards: []
+  })
+
+  const getData = async () => {
+    // vacío la mano anterior de cartas
+    try {
+      // genero una mano de 'n' cantidad de cartas aleatorias
+      let cards = []
+      for (let i = 0; i < n; i++) {
+        const res = await axios(`https://pokeapi.co/api/v2/pokemon/${generateRandom(1, 898)}`)
+        cards = [...cards,
+          {
+            pokemonID: res.data.id,
+            name: res.data.name,
+            image: res.data.sprites.other.home.front_default,
+            type: res.data.types[0].type.name,
+            className: 'card',
+            selected: false,
+            disabled: false
+          }
+        ]
+      }
+      // duplico las cartas y las mezclo
+      const shuffledCards = shuffleArray([...cards, ...cards])
+      setData({
+        loading: false,
         error: null,
-        cards: []
-    });
-
-    const getData = async () => {
-        //vacío la mano anterior de cartas
-        try {
-            //genero una mano de 'n' cantidad de cartas aleatorias
-            var cards = [];
-            for (let i = 0; i < n; i++) {
-                let res = await axios(`https://pokeapi.co/api/v2/pokemon/${generateRandom(1, 898)}`)
-                cards = [...cards,
-                {
-                    pokemonID: res.data.id,
-                    name: res.data.name,
-                    image: res.data.sprites.other.home.front_default,
-                    type: res.data.types[0].type.name,
-                    className: "card",
-                    selected: false,
-                    disabled: false
-                }
-                ];
-            }
-            //duplico las cartas y las mezclo
-            const shuffledCards = shuffleArray([...cards, ...cards])
-            setData({
-                loading: false,
-                error: null,
-                cards: shuffledCards
-            });
-        }
-        catch (error) {
-            console.log(error);
-        }
+        cards: shuffledCards
+      })
+    } catch (error) {
+      console.log(error)
     }
+  }
 
+  useEffect(() => {
+    // vacío la lista de cartas
+    setData({
+      loading: true,
+      error: null,
+      cards: []
+    })
+    // llamo a la función que trae 'n' cantidad de cartas
+    getData()
+  }, [handler])
 
-    useEffect(() => {
-        //vacío la lista de cartas
-        setData({
-            loading: true,
-            error: null,
-            cards: []
-        });
-        //llamo a la función que trae 'n' cantidad de cartas
-        getData();
-
-    }, [handler])
-
-
-    return [data.cards, data.loading, data.error];
+  return [data.cards, data.loading, data.error]
 }
